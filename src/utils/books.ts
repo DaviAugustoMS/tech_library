e14ec943149d914d3e8b6cef12415e10eed349bc
DaviAugustoMS <daviaugusto.developer@gmail.com>
2021-12-09 15:02:42 -0300

    feat: create books

---
+++ src/utils/books.ts
@@ -0,0 +1,59 @@
+const books = [
+  {
+    id: 1,
+    type: "A",
+    color: "yellow",
+    size: "5",
+  },
+  {
+    id: 2,
+    type: "B",
+    color: "red",
+    size: "3",
+  },
+  {
+    id: 3,
+    type: "C",
+    color: "orange",
+    size: "7",
+  },
+  {
+    id: 4,
+    type: "D",
+    color: "purple",
+    size: "8",
+  },
+  {
+    id: 5,
+    type: "E",
+    color: "lightblue",
+    size: "1",
+  },
+  {
+    id: 6,
+    type: "F",
+    color: "pink",
+    size: "2",
+  },
+  {
+    id: 7,
+    type: "G",
+    color: "lightpink",
+    size: "2",
+  },
+
+  {
+    id: 8,
+    type: "H",
+    color: "blue",
+    size: "6",
+  },
+  {
+    id: 9,
+    type: "I",
+    color: "lightgreen",
+    size: "4",
+  },
+];
+
+export default books;
