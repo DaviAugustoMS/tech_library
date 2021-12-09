21bf2dcf87d8183f1aeec9eccaf137480751b661
DaviAugustoMS <daviaugusto.developer@gmail.com>
2021-12-09 15:02:03 -0300

    feat: create logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,10 @@
+import "./styles.scss";
+const Logo = () => {
+  return (
+    <div className="logo-container">
+      <div className="logo-container__logo" />
+    </div>
+  );
+};
+
+export { Logo };
